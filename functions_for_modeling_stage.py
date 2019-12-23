ebb74195b9fc8495e22933de934688317d3a723c
moti ben harosh <moti@vayusense.com>
2019-12-23 14:16:43 +0200

    with gold function

--- functions_for_modeling_stage.py
+++ functions_for_modeling_stage.py
@@ -34,7 +34,7 @@ def run_var_model(variable, paramComb, trainData, testData):
         zout1, wout = loess_nd_test_point(relTrainData, trainDistVar, trainResults,
                                       relTestData, testDistVar, frac=paramComb['frac'])
 
-        errorVal += abs(zout1 - testResult)/(zout1 + testResult)
+        errorVal += abs(zout1 - testResult)/(abs(zout1) + abs(testResult))
     return errorVal
             # np.mean(np.abs((z_smoot_test[:, idxFeat, idxFilt] - titterTestNP) / (titterTestNP + z_smoot_test[:, idxFeat, idxFilt])))
 
@@ -226,7 +226,7 @@ def run_and_test_full_model(pref, results, trainData, interpDataPP):
                 nameVec.append(currModelNames[idx])
         currModelState = currModelStateInit.iloc[nameIdx]
 
-        t_end=20#modeledVars[exp].index[-1]
+        t_end=modeledVars[exp].index[-1]
         # run model every time step for all modeled variables
         for t in range(0, t_end, Settings['DT']):  #  start from t=1[minutes]
             currModelState[pref['Data variables']] =\
@@ -247,17 +247,23 @@ def run_and_test_full_model(pref, results, trainData, interpDataPP):
                 except:
                     q=2
                 currModelState[var] = modeledVars[exp][var].iloc[t+1]
-    gold_var=gold_hyper(pref,testData,modeledVars)
-    return modeledVars
+    gold_mean=gold_hyper(pref,testData,modeledVars)
+    return modeledVars, gold_mean
 def gold_hyper(pref,testData,modeledVars):
+    # function gold_hyper calculate the goodness of predicted model (modeledVars) to test data (testData)
+   # input:
     gold_var={}
+    gold_mean_comul=0
     for exp in testData.keys():
         #expLength = int(round(testData[exp]['TimeMeas'].iloc[-1])) + 1 #  Experiments length in minutes
         gold_var[exp] = pd.DataFrame()
         for var in pref['Variables']:
-           # modeledVarsForExp[var] = testData[exp][var].iloc[0] * np.ones([expLength, ])
-            gold_var[exp][var]=[sum(abs((testData[exp][var]-modeledVars[exp][var])/(testData[exp][var]+modeledVars[exp][var])))]
-    return gold_var
+           # calculte goodness of prediction  for spesific exp and specific featchare
+            gold_var[exp][var]=[sum(abs((testData[exp][var]-modeledVars[exp][var])/(abs(testData[exp][var])+abs(modeledVars[exp][var]))))/len(testData[exp][var])]
+        gold_mean_comul=gold_mean_comul+gold_var[exp].mean(axis=1) # assuming uniform weights for the featchers
+        
+    gold_mean=gold_mean_comul/len(testData.keys())
+    return gold_mean
 def simulation_initialization(expData, pref):
     # Settings
     Settings = dict()
--- functions_for_score_and_display.py
+++ functions_for_score_and_display.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-def show_results(modeledVars, interpDataPP, pref):
+def show_results(modeledVars, interpDataPP, pref,gold_mean):
     testData = {exp: interpDataPP[exp] for exp in pref['CVTest']}
     numOfVar = len(pref['Variables'])
     numOfRows = int(math.ceil(numOfVar/2))
@@ -11,6 +11,6 @@ def show_results(modeledVars, interpDataPP, pref):
         for varIdx, varName in enumerate(pref['Variables'], start=1):
             plt.subplot(numOfRows, 2, varIdx)
             plt.plot(testData[exp]['TimeMeas'], testData[exp][varName], modeledVars[exp][varName])
-            plt.title(varName)
+            plt.title([varName+', gold='+str(gold_mean)])
             plt.legend(['Modeled data', 'Measured data'])
 
